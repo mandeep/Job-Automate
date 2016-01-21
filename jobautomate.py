3270840aa7e9ca694ea2b04e8f10e4e964fcb9fe
Mandeep Bhutani <bhutanimandeep@gmail.com>
2016-01-21 11:50:26 -0600

    Split apply function into two functions. Verified PhantomJS cooperation.

--- jobautomate.py
+++ jobautomate.py
@@ -10,12 +10,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 job = input('Please enter a job:')
 city = input('Please enter a city:')
 
-driver = webdriver.Firefox()
+driver = webdriver.PhantomJS()
 
 
 def initiate_search():
@@ -33,35 +33,40 @@ def sort_results():
     driver.find_element_by_link_text('date').click()
 
 
+def click_job():
+    """Click the link that redirects to the job application."""
+    driver.find_element_by_xpath("//a[@data-tn-element='jobTitle']").click()
+
+
 def switch_window():
     """Switch windows to the newly opened job opening window."""
     driver.switch_to.window(driver.window_handles[1])
 
 
-def apply_to_job():
+def click_apply():
     """Click the Indeed apply button to easily apply to the job.
     If the Indeed apply button does not exist, the new window closes."""
     try:
         driver.find_element_by_class_name('indeed-apply-button').click()
-        wait = WebDriverWait(driver, 10)
-        frame = wait.until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, "iframe[name$=modal-iframe]")))
-        driver.switch_to.frame(frame)
-        driver.switch_to.frame(0)
-        driver.find_element_by_id('applicant.name').send_keys('full name')
-        driver.find_element_by_id('applicant.email').send_keys('email')
     except NoSuchElementException:
         driver.close()
 
-def click_job():
-    """Click the link that redirects to the job application."""
-    links = driver.find_elements_by_xpath("//a[@data-tn-element='jobTitle']")
-    for link in links:
-        link.click()
-        apply_to_job()
+
+def apply_to_job():
+    wait = WebDriverWait(driver, 10)
+    frame = wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, "iframe[name$=modal-iframe]")))
+    driver.switch_to.frame(frame)
+    driver.switch_to.frame(0)
+    driver.implicitly_wait(5)
+    driver.find_element_by_id('applicant.name').send_keys('full name')
+    driver.find_element_by_id('applicant.email').send_keys('email')
 
 
 if __name__ == "__main__":
     initiate_search()
     sort_results()
     click_job()
+    switch_window()
+    click_apply()
+    apply_to_job()
