8fdda8f48cb52a8f28df4ff496ddcb50e8fbf060
Mandeep Bhutani <bhutanimandeep@gmail.com>
2016-01-20 20:29:32 -0600

    Added lines to apply function.

--- jobautomate.py
+++ jobautomate.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 job = input('Please enter a job:')
 city = input('Please enter a city:')
@@ -32,13 +33,6 @@ def sort_results():
     driver.find_element_by_link_text('date').click()
 
 
-def click_job():
-    """Click the link that redirects to the job application."""
-    links = driver.find_elements_by_xpath("//a[@data-tn-element='jobTitle']")
-    for link in links:
-        link.click()
-
-
 def switch_window():
     """Switch windows to the newly opened job opening window."""
     driver.switch_to.window(driver.window_handles[1])
@@ -56,13 +50,18 @@ def apply_to_job():
         driver.switch_to.frame(0)
         driver.find_element_by_id('applicant.name').send_keys('full name')
         driver.find_element_by_id('applicant.email').send_keys('email')
-    except:
+    except NoSuchElementException:
         driver.close()
+
+def click_job():
+    """Click the link that redirects to the job application."""
+    links = driver.find_elements_by_xpath("//a[@data-tn-element='jobTitle']")
+    for link in links:
+        link.click()
+        apply_to_job()
+
 
 if __name__ == "__main__":
     initiate_search()
     sort_results()
     click_job()
-    while len(driver.window_handles) > 1:
-        switch_window()
-        apply_to_job()
