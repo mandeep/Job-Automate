f8541edd354e78a81ca951acfac3fbe758723fb0
mandeepbhutani <bhutanimandeep@gmail.com>
2016-01-22 19:10:25 -0600

    Added docstring

--- jobautomate.py
+++ jobautomate.py
@@ -59,8 +59,9 @@ def switch_frames():
 
 
 def fill_application():
-    """Indeed gives employers four applications to choose from. Some fields
-    are not required; however, they are filled just in case."""
+    """Indeed gives employers four applications to choose from. The function
+    uses try/except to identify the applicatoin Some fields are not required; 
+    however, they are filled just in case."""
     try:
         driver.find_element_by_id('applicant.name').send_keys(first_name + " " + last_name)
         driver.find_element_by_id('applicant.email').send_keys(email_address)
