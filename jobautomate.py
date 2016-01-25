e581b04377a291d2ff7900cc359b93e8142b1a3b
mandeepbhutani <bhutanimandeep@gmail.com>
2016-01-24 22:30:15 -0600

    Added new indeed_search() function with docstring

--- jobautomate.py
+++ jobautomate.py
@@ -9,24 +9,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
 
 
-client = IndeedClient('7458209865285883')
+def indeed_search():
+    """Use Indeed API to obtain job application links.
+    :param q: job description
+    :param l: job location
+    :param sort: sort results by relevance or date
+    :param sr: direct hire only or include staffing agencies
+    :param limit: number of results, 0-25
+    :param fromage: number of days to search
+    :param start: index of beginning search result
+    :param userip: ip of client (required)
+    :param useragent: user agent of client (required)
+    """
+    client = IndeedClient('7458209865285883')
+    params = {'q': 'analyst',
+              'l': "",
+              'sort': 'date',
+              'sr': 'directhire',
+              'limit': 25,
+              'fromage': 30,
+              'start': 0,
+              'userip': "1.2.3.4",
+              'useragent': "Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/43.0"
+              }
+    response = client.search(**params)
+    urls = [str(links['url']) for links in response['results']]
+    return urls
 
-params = {'q': 'analyst',
-          'l': "",
-          'sort': 'date',
-          'sr': 'directhire',
-          'limit': 25,
-          'fromage': 30,
-          'start': 0,
-          'userip': "1.2.3.4",
-          'useragent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)"
-          }
-
-response = client.search(**params)
-
-urls = [str(links['url']) for links in response['results']]
-
-driver = webdriver.PhantomJS()
+driver = webdriver.Firefox()
 
 with open('information.txt', 'r') as file:
     data = file.read().replace('\n', '').split(',')
