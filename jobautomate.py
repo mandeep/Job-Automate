05a1e23c6336d32bf43aff667b203ad43f73a278
mandeepbhutani <bhutanimandeep@gmail.com>
2016-01-24 22:30:53 -0600

    Removed unused imports

--- jobautomate.py
+++ jobautomate.py
@@ -2,8 +2,6 @@ import os
 from indeed import IndeedClient
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
