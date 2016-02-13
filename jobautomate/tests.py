83e7fb60374fc26bb739287ef4362de268a32def
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-12 23:07:23 -0600

    Started unit tests for jobautomate

---
+++ jobautomate/tests.py
@@ -0,0 +1,30 @@
+import unittest
+from indeed import IndeedClient
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from jobautomate import indeed_parameters, indeed_urls, switch_frames
+
+
+class JobAutomateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_parameters(self):
+        self.assertIsNotNone(indeed_parameters)
+
+    def test_api(self):
+        self.assertTrue(len(indeed_urls(indeed_parameters("python developer", ""))) > 1)
+
+    def test_api_again(self):
+        self.assertFalse(len(indeed_urls(indeed_parameters("job_does_not_exist", "location_does_not_exit"))) > 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
