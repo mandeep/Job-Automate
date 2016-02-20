1307b84cee0db714dd39a4ad80b565df8e837a52
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-20 11:59:36 -0600

    Added functionality to run the jobautomate script from user web input

--- jobautomate/web_search/views.py
+++ jobautomate/web_search/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import jobautomate
+from jobautomate import run_script
 
 
 def index(request):
     context = {}
-    request.POST.get("job_title", "job_location")
+    request.POST.get('job_title', 'job_location')
+    if request.POST.get('run'):
+        what = request.POST['job_title']
+        where = request.POST['job_location']
+        run_script(what, where)
     return render(request, 'web_search/index.html', context)
