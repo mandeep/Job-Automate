257ecdfa6ecc1eae586038f0b335d667d06bae77
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-21 19:04:23 -0600

    Working to beautify the front end

--- jobautomate/web_app/settings.py
+++ jobautomate/web_app/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
