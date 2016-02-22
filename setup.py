a6f9247688bc91d80358ae476e7c9880f32296ea
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-21 23:26:43 -0600

    Added new dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(name='jobautomate',
       install_requires=[
         'indeed',
         'selenium',
+        'django'
+        'requests'
       ],
       entry_points='''
         [console_scripts]
