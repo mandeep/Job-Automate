c4c11c57e5aed97f2119ad0491c7eb0bb7b7074a
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-22 20:56:44 -0600

    Added more input form requests

--- jobautomate/web_search/views.py
+++ jobautomate/web_search/views.py
@@ -4,9 +4,12 @@ import jobautomate
 
 def index(request):
     context = {}
-    request.POST.get('job_title', 'job_location')
     if request.POST.get('run'):
         what = request.POST['job_title']
         where = request.POST['job_location']
-        jobautomate.run_script(what, where)
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        resume = request.FILES['resume'].read()
+        jobautomate.run_script(what, where, resume, first_name, last_name, email)
     return render(request, 'web_search/index.html', context)
