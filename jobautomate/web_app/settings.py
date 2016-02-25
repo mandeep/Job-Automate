be8a020d3bc9a091816ad8861f9c03970f2b3799
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-25 12:11:42 -0600

    Added static setting for heroku

--- jobautomate/web_app/settings.py
+++ jobautomate/web_app/settings.py
@@ -116,8 +116,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
