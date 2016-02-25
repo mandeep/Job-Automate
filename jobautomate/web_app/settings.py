ab5dee5da488da3171c357c9db578c4ab3757242
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-25 12:15:37 -0600

    Fixed CSS typo in settings

--- jobautomate/web_app/settings.py
+++ jobautomate/web_app/settings.py
@@ -121,7 +121,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
