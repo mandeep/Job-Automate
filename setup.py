fcd28e0d587ff7db92b50b7fc78307179846ac33
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-26 11:17:24 -0600

    Changed version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='jobautomate',
-      version='0.0.2',
+      version='0.0.3',
       author='Mandeep Bhutani',
       packages=find_packages(),
       include_package_data=True,
