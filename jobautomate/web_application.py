058058edc6e5b3711a9497d167232903797d750f
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-26 11:20:24 -0600

    Commit for next heroku release

--- jobautomate/web_application.py
+++ jobautomate/web_application.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
 
+
 driver = webdriver.PhantomJS()
 
 
