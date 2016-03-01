2637022e4a75228580c3ad97296df373fdfa9846
mandeepbhutani <bhutanimandeep@gmail.com>
2016-02-29 20:11:29 -0600

    Removed secret key

--- jobautomate/web_app/settings.py
+++ jobautomate/web_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*jnlbokamc6ibpo)f1o*yry!bt9k#tv7j&=)%t2q0o+i-m1pnc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
