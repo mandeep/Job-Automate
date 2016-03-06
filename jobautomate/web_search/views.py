6e46c48c3aee8e1d2a3f498e40ec7f8387fec9d5
mandeepbhutani <bhutanimandeep@gmail.com>
2016-03-06 15:20:45 -0600

    Removed forms in html and added forms.py forms to views.py

--- jobautomate/web_search/views.py
+++ jobautomate/web_search/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from jobautomate.web_application import django_view
+from .forms import NameForm, EmailForm, UploadForm, JobSearchForm
 
 
 def index(request):
-    context = {}
-    if request.POST.get('run'):
-        what = request.POST['job_title']
-        where = request.POST['job_location']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        resume = request.FILES['resume'].read()
-        django_view(what, where, first_name,
-                    last_name, email, resume)
-    return render(request, 'web_search/index.html', context)
+    if request.method == 'POST':
+        name_form = NameForm(request.POST)
+        email_form = EmailForm(request.POST)
+        upload_form = UploadForm(request.POST)
+        job_search_form = JobSearchForm(request.POST)
+        if name_form.is_valid() and email_form.is_valid() \
+                and upload_form.is_valid() and job_search_form.is_valid():
+            first_name = name_form.cleaned_data['first_name']
+            last_name = name_form.cleaned_data['last_name']
+            email = email_form.cleaned_data['email']
+            resume = upload_form.cleaned_data['resume_upload']
+            what = job_search_form.cleaned_data['job_description']
+            where = job_search_form.cleaned_data['job_location']
+            django_view(what, where, first_name, last_name, email, resume)
+    else:
+        name_form = NameForm()
+        email_form = EmailForm()
+        upload_form = UploadForm()
+        job_search_form = JobSearchForm()
+    return render(request, 'web_search/index.html',
+                  {'name_form': name_form, 'email_form': email_form,
+                   'upload_form': upload_form, 'job_search_form': job_search_form})
