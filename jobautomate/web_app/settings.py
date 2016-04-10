daf7a8422a91af84ace15380690ff61e7d6f477b
mandeepbhutani <bhutanimandeep@gmail.com>
2016-04-09 22:44:52 -0500

    Changed staticfiles_dirs for heroku

--- jobautomate/web_app/settings.py
+++ jobautomate/web_app/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/web_search/static'),
+    os.path.join(BASE_DIR, '/app/jobautomate/web_search/static'),
 )
