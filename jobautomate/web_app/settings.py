f430a0cb2523ed2400c2d35f4012ec57bee7e000
mandeepbhutani <bhutanimandeep@gmail.com>
2016-04-09 22:43:43 -0500

    Changed staticdirs

--- jobautomate/web_app/settings.py
+++ jobautomate/web_app/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, '/web_search/static'),
 )
