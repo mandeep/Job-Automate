ad68e9436b3bbdf17261569146a5caa7bad1cfb4
mandeepbhutani <bhutanimandeep@gmail.com>
2016-04-27 19:20:47 -0500

    Called resume into Resume model

--- jobautomate/web_search/views.py
+++ jobautomate/web_search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from jobautomate.web_application import django_view
 from .forms import NameForm, EmailForm, UploadForm, JobSearchForm
+from .models import Resume
 
 
 def index(request):
@@ -15,7 +16,7 @@ def index(request):
             first_name = name_form.cleaned_data['first_name']
             last_name = name_form.cleaned_data['last_name']
             email = email_form.cleaned_data['email']
-            resume = request.FILES['resume_upload']
+            resume = Resume(request.FILES['resume_upload'])
             what = job_search_form.cleaned_data['job_description']
             where = job_search_form.cleaned_data['job_location']
             django_view(what, where, first_name, last_name, email, resume)
