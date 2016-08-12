63fdf5c87bcc4a49052956b769b734c04ba39171
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-11 19:09:10 -0500

    Changed method names to not conflict with cli names

--- jobautomate/tests/test_application.py
+++ jobautomate/tests/test_application.py
@@ -5,20 +5,20 @@ import pytest
 
 class TestJobAutomate:
 
-    def test_indeed_parameters(self):
+    def test_indeed_api_parameters(self):
         parameters = jobautomate.commandline.indeed_parameters('Financial Analyst', 'New York City')
         assert isinstance(parameters, dict) is True
         assert 'Financial Analyst' in parameters.values()
         assert 'New York City' in parameters.values()
 
-    def test_indeed_urls(self):
+    def test_indeed_api_urls(self):
         parameters = jobautomate.commandline.indeed_parameters('Financial Analyst', 'New York City')
         job_urls = jobautomate.commandline.indeed_urls(parameters)
         assert len(job_urls) > 1
         for url in job_urls:
             assert 'http://' in url
 
-    def test_apply_button(self, selenium):
+    def test_indeed_apply_button(self, selenium):
         selenium.get(
             "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
         selenium.implicitly_wait(7)
