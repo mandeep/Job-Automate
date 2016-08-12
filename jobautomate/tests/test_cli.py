5dd0b1f69e4622ec7771d61dae6a264cfb447160
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-11 19:47:29 -0500

    Added fill application test

--- jobautomate/tests/test_cli.py
+++ jobautomate/tests/test_cli.py
@@ -1,5 +1,5 @@
 import jobautomate.commandline
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
 import pytest
 
 
@@ -26,3 +26,17 @@ class TestJobAutomate:
             jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
         except NoSuchElementException:
             pytest.fail('NoSuchElementException')
+
+    def test_fill_application(self, selenium):
+        selenium.get(
+            "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
+        selenium.implicitly_wait(7)
+        try:
+            jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
+            selenium.implicitly_wait(5)
+            jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
+            selenium.implicitly_wait(5)
+            jobautomate.commandline.fill_application(
+                selenium, 'Homer', 'Simpson', 'Chunkylover53Aoo.com', 'resume.txt')
+        except (NoSuchElementException, ElementNotVisibleException):
+            pytest.fail('Application not found.')
