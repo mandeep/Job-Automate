9127e7b7748c6e113eb285180c44ad61e35ae7b9
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-19 04:32:51 -0500

    Added pytest fixture

--- jobautomate/tests/test_cli.py
+++ jobautomate/tests/test_cli.py
@@ -1,93 +1,101 @@
 from click.testing import CliRunner
 import jobautomate.commandline
+import pytest
 
 
-class TestJobAutomate:
+@pytest.fixture
+def parameters():
+    job_search = jobautomate.commandline.indeed_parameters('Financial Analyst', 'New York City')
+    return job_search
 
-    def test_indeed_api_parameters(self):
-        parameters = jobautomate.commandline.indeed_parameters('Financial Analyst', 'New York City')
-        assert isinstance(parameters, dict) is True
-        assert 'Financial Analyst' in parameters.values()
-        assert 'New York City' in parameters.values()
 
-    def test_indeed_api_urls(self):
-        parameters = jobautomate.commandline.indeed_parameters('Financial Analyst', 'New York City')
-        job_urls = jobautomate.commandline.indeed_urls(parameters)
-        assert len(job_urls) == 25
-        for url in job_urls:
-            assert 'http://' in url
+def test_indeed_api_parameters(parameters):
+    assert isinstance(parameters, dict) is True
+    assert 'Financial Analyst' in parameters.values()
+    assert 'New York City' in parameters.values()
 
-    def test_false_api_key(self):
-        parameters = jobautomate.commandline.indeed_parameters('Financial Analyst', 'New York City')
-        try:
-            jobautomate.commandline.indeed_urls(parameters, 2)
-        except NameError:
-            pass
 
-    def test_indeed_apply_button(self, selenium):
-        selenium.get(
-            "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
-        selenium.implicitly_wait(7)
-        jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
+def test_indeed_api_urls(parameters):
+    job_urls = jobautomate.commandline.indeed_urls(parameters)
+    assert len(job_urls) == 25
+    for url in job_urls:
+        assert 'http://' in url
 
-    def test_fill_application(self, selenium):
-        selenium.get(
-            "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
-        selenium.implicitly_wait(7)
-        jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
-        jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
-        jobautomate.commandline.fill_application(
-            selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
 
-    def test_fill_application_again(self, selenium):
-        selenium.get(
-            "http://www.indeed.com/viewjob?jk=f6c24d6728d6b55f")
-        selenium.implicitly_wait(7)
-        jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
+def test_false_api_key(parameters):
+    try:
+        jobautomate.commandline.indeed_urls(parameters, 2)
+    except NameError:
+        pass
 
-        jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
-        jobautomate.commandline.fill_application(
-            selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
 
-    def test_click_apply(self, selenium):
-        selenium.get(
-            "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
-        selenium.implicitly_wait(7)
-        jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
-        jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
-        jobautomate.commandline.fill_application(
-            selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
-        jobautomate.commandline.apply_or_continue(selenium, debug=True)
+def test_indeed_apply_button(selenium):
+    selenium.get(
+        "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
+    jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
 
-    def test_click_apply_again(self, selenium):
-        selenium.get(
-            "http://www.indeed.com/viewjob?jk=f6c24d6728d6b55f")
-        selenium.implicitly_wait(7)
-        jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
 
-        jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
-        jobautomate.commandline.fill_application(
-            selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
-        jobautomate.commandline.apply_or_continue(selenium, debug=True)
+def test_fill_application(selenium):
+    selenium.get(
+        "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
+    jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
+    jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
+    jobautomate.commandline.fill_application(
+        selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
 
-    def test_cli_command(self):
-        runner = CliRunner()
-        result = runner.invoke(
-            jobautomate.commandline.cli, ['--debug', 'Homer', 'Simpson', 'Chunkylover53@aol.com',
-                                          'Nuclear Technician', '.travis.yml'], input='No')
-        assert not result.exception
 
-    def test_cli_verbose(self):
-        runner = CliRunner()
-        verbose_result = runner.invoke(
-            jobautomate.commandline.cli, ['--debug', '--verbose', 'Homer', 'Simpson',
-                                          'Chunkylover53@aol.com', 'Nuclear Technician',
-                                          '.travis.yml'], input='No')
-        assert not verbose_result.exception
+def test_fill_application_again(selenium):
+    selenium.get(
+        "http://www.indeed.com/viewjob?jk=f6c24d6728d6b55f")
+    selenium.implicitly_wait(7)
+    jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
 
-    def test_cli_continue(self):
-        runner = CliRunner()
-        continue_result = runner.invoke(
-            jobautomate.commandline.cli, ['--debug', 'Homer', 'Simpson', 'Chunkylover53@aol.com',
-                                          'Nuclear Technician', '.travis.yml'], input='Yes')
-        assert not continue_result.exception
+    jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
+    jobautomate.commandline.fill_application(
+        selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
+
+
+def test_click_apply(selenium):
+    selenium.get(
+        "http://www.indeed.com/cmp/Discover-Books/jobs/Full-Time-Driver-3ce56851d8772139")
+    jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
+    jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
+    jobautomate.commandline.fill_application(
+        selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
+    jobautomate.commandline.apply_or_continue(selenium, debug=True)
+
+
+def test_click_apply_again(selenium):
+    selenium.get(
+        "http://www.indeed.com/viewjob?jk=f6c24d6728d6b55f")
+    jobautomate.commandline.find_apply_button(selenium, 'indeed-apply-button')
+
+    jobautomate.commandline.switch_frames(selenium, 'iframe[name$=modal-iframe]')
+    jobautomate.commandline.fill_application(
+        selenium, 'Homer', 'Simpson', 'Chunkylover53@aol.com', 'resume.txt')
+    jobautomate.commandline.apply_or_continue(selenium, debug=True)
+
+
+def test_cli_command():
+    runner = CliRunner()
+    result = runner.invoke(
+        jobautomate.commandline.cli, ['--debug', 'Homer', 'Simpson', 'Chunkylover53@aol.com',
+                                      'Nuclear Technician', '.travis.yml'], input='No')
+    assert not result.exception
+
+
+def test_cli_verbose():
+    runner = CliRunner()
+    verbose_result = runner.invoke(
+        jobautomate.commandline.cli, ['--debug', '--verbose', 'Homer', 'Simpson',
+                                      'Chunkylover53@aol.com', 'Nuclear Technician',
+                                      '.travis.yml'], input='No')
+    assert not verbose_result.exception
+
+
+def test_cli_continue():
+    runner = CliRunner()
+    continue_result = runner.invoke(
+        jobautomate.commandline.cli, ['--debug', 'Homer', 'Simpson', 'Chunkylover53@aol.com',
+                                      'Nuclear Technician', '.travis.yml'], input='Yes')
+    assert not continue_result.exception
