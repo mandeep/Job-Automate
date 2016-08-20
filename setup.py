58b0bd7810a56d807544cf27f23c458d7f7d26a0
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-20 16:34:19 -0500

    Added classifiers

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='jobautomate',
       version='1.0.1',
       author='Mandeep Bhutani',
       packages=['jobautomate', 'jobautomate.tests'],
+      license='GPLv3+',
       install_requires=[
         'indeed',
         'selenium',
@@ -14,4 +15,10 @@ setup(name='jobautomate',
         [console_scripts]
         jobautomate=jobautomate.commandline:cli
         ''',
+      classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+      ]
       )
