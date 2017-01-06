22128df9d2b9fdf7fa4f0aee7deb2f4447fb9dc1
mandeep <mandeep@users.noreply.github.com>
2017-01-06 01:45:25 -0600

    0.18.6 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jobautomate',
-      version='0.18.5',
+      version='0.18.6',
       author='Mandeep Bhutani',
       description='Automate a job search with Indeed',
       url='https://github.com/mandeep/Job-Automate',
